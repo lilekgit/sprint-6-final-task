1aacf1bfb2571f9589e2f087fdbe73e54a7f5bc5
Maksim Glushchenko <gluschenko.maks@icloud.com>
2026-06-05 22:04:30 +0300

    Реализация финального проекта спринта 6

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,35 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"sprint6finaltask/internal/handlers"
+)
+
+type Server struct {
+	Logger *log.Logger
+	HTTP   *http.Server
+}
+
+func NewServer(logger *log.Logger) *Server {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", handlers.HomeHandler)
+	mux.HandleFunc("/upload", handlers.UploadHandler)
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	return &Server{
+		Logger: logger,
+		HTTP:   srv,
+	}
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,34 @@
 package service
+
+import (
+	"strings"
+
+	"sprint6finaltask/pkg/morse"
+)
+
+func isMorse(s string) bool {
+	hasDotOrDash := false
+	for _, r := range s {
+		switch r {
+		case '.', '-':
+			hasDotOrDash = true
+		case ' ':
+			// допустимый символ
+		default:
+			return false
+		}
+	}
+	return hasDotOrDash
+}
+
+func AutoConvert(input string) (string, error) {
+	input = strings.TrimSpace(input)
+	if input == "" {
+		return "", nil
+	}
+
+	if isMorse(input) {
+		return morse.ToText(input), nil
+	}
+	return morse.ToMorse(input), nil
+}
